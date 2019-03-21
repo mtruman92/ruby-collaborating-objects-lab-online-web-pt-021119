c748b6a722fd8405918785e90bd809ff3ccfa3d0
Mickae Truman <mtruman@nyit.edu>
2019-03-21 16:05:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,47 +1,51 @@
-class Artist 
-  
-  attr_accessor :name, :songs
-  
-  
+class Artist
+
+  attr_accessor :name, :artist, :songs
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
   end
-  
+
   def add_song(song)
-    @songs << song 
+    @songs << song
   end
-  
+
   def songs
     @songs
   end
-  
+
   def save
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || Artist.new(name)
-end
-
-  def self.find_by_name(name)
-    self.all.detect{|a| a.name == name}
+    #binding.pry
+      if self.find(name)
+        self.find(name)
+      else
+        self.create(name)
+      end
   end
 
-  def self.create_by_name(name)
-    #Artist.new(name) - need to find what code would work with this to save
-    self.new(name).tap {|artist| artist.save}
+  def self.find(name)
+    self.all.find {|artist| artist.name == name}
   end
-  
+
+  def self.create(name)
+    new_var = self.new(name)
+    new_var.save
+    return new_var
+  end
+
   def print_songs
-    songs.each {|song| puts song.name}
-  end
-  
+    self.songs.each {|song| puts song.name}
 
-end 
+  end
+end
