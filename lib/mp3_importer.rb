d3a490c8b03798a32f5a53cbed4e6b5e948406ce
Mickae Truman <mtruman@nyit.edu>
2019-03-21 16:04:41 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,18 +1,19 @@
 class MP3Importer
-  
- attr_accessor :path
- 
+  attr_accessor :path
   def initialize(path)
     @path = path
   end
-  
+
   def files
-  files = Dir.entries(@path)
-  files.keep_if{|file| file.end_with?(".mp3")}
-end
+    @files = Dir.entries(path)
+    @files.delete_if { |file| file == "." || file == ".." }
+  end
 
-def import 
-  files.each {|file|Song.new_by_filename()}
-end
-
+  def import
+    #@files.split (" - ")
+      self.files.each do |file|
+        Song.new_by_filename(file)
+        #binding.pry
+      end
+  end
 end
