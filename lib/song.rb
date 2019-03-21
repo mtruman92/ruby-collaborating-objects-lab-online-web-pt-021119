4b98cf267e2815a9f914463bcc83b7f42b9dcc80
Mickae Truman <mtruman@nyit.edu>
2019-03-21 15:53:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,18 @@ class Song
     #song
  #end
   
-  
+   def self.new_by_filename(file_name)
+    file = file_name.chomp(".mp3").split(" - ")
+    song = self.new(file[1])
+    song.artist_name=(file[0])
+    song
+  end
+
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end 
   
 
 
