a6f3d209a1765c5caad2849bebda26028b3d4123
Mickae Truman <mtruman@nyit.edu>
2019-03-21 15:45:11 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+  
+ attr_accessor :path
+ 
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+  files = Dir.entries(@path)
+  files.keep_if{|file| file.end_with?(".mp3")}
+end
+
+def import 
+  files.each {|file|Song.new_by_filename()}
+end
+
+end
